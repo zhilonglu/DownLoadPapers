7b577db2c88c223abd23aef7ab72c85e3f2bdbdf
zhilonglu <zhilong_lu@buaa.edu.cn>
2017-12-20 15:58:27 +0800

    add
    修改

--- downloadPDF_IJcai.py
+++ downloadPDF_IJcai.py
@@ -1,29 +1,35 @@
 # -*-coding:utf-8-*-
 import urllib.request
 from bs4 import BeautifulSoup
-import re
 import os
 import io
 import sys
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')
-path = "D:\\PaperLists\\AI\\IJCAI\\"
+path = "E:\\PaperLists\\AI\\IJCAI\\"
 
 # url = "http://ijcai-16.org/index.php/accepted-papers#main-track"
-url = "http://ijcai-16.org/index.php/welcome/view/accepted_papers"
+url = "http://www.ijcai.org/proceedings/2013/"
 content = urllib.request.urlopen(url).read()
 soup = BeautifulSoup(content, "html.parser")
-Body = soup.find_all("li")
-if not os.path.exists(path + "IJCAI2016\\"):
-    os.mkdir(path + "IJCAI2016\\")
-count = 0
-with open(path + "IJCAI2016\\" + "2016_acceptedPaper_IJCAI.txt", "w", encoding='utf-8') as f:
-    for i in range(len(Body)):
-        if str(Body[i].contents[0].name) == 'b':
-            try:
-                count += 1
-                name = str(Body[i].contents[0].text)
-                authors = str(Body[i].contents[1].text).replace("&nbsp","").replace(",","#")
-                f.write(str(count) + "," + name + "," + authors + "\n")
-            except:
-                continue
+title = soup.find_all('p')
+# details = soup.find_all('a')
+if not os.path.exists(path + "IJCAI2013\\"):
+    os.mkdir(path + "IJCAI2013\\")
+for i in range(13,len(title)):
+    try:
+        file_pdf = title[i].contents[0]['href']
+        file_title = title[i].contents[0].text.replace("\"", "_").replace(":", "_").replace("*", "_").replace("?","_").replace("/", "_")
+        filename = ""
+        if not os.path.exists(path + "IJCAI2013\\" + file_title + ".pdf"):
+            filename = path + "IJCAI2013\\2013_IJCAI_" + file_title + ".pdf"
+        else:
+            continue
+        f = open(filename, 'wb')
+        # pre_url = "http://www.ijcai.org/"
+        paper = urllib.request.urlopen(file_pdf)
+        f.write(paper.read())
+        f.close()
+    except:
+        # print("%d was not done!" % (i - 5))
+        print(str(file_pdf)+" was not done!")
